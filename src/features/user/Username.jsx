c7f45abb4e9378c56286cecfb96a34052e7aac53
KNXCSdev <michal.miksho@gmail.com>
2025-01-03 17:36:53 +0100

    Styling buttons,text,Header,Home,AppLayout

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Michal</div>;
+  return <div className="hidden text-sm font-semibold md:block">Michal</div>;
 }
 
 export default Username;
